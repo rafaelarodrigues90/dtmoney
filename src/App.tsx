7e2980ed3c025d2c828eeb788f7a3c8dfe928123
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2022-07-03 17:02:02 -0300

    dtmoney project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsTable } from "./components/TransactionsTable";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal'
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModalOpen() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModalOpen() {
+        setIsNewTransactionModalOpen(false)
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModalOpen} />  
       <GlobalStyle />
+      <TransactionsTable />
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOMClient from 'react-dom/client';
 import { App } from './App';
+import { createServer, Model } from 'miragejs'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOMClient.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 500,
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
+  }
+})
